2f0886fbdfdf4ccd0c7bbb4a2d72613dc0bf5354
Himanshu <gopihimanshu@gmail.com>
2021-09-25 17:26:06 +0530

    Added the basic redux functionality and now I am able to fetch data from store not from local state

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 import CounterControl from "../../components/CounterControl/CounterControl";
+import * as actionTypes from "../../store/actions";
 
 class Counter extends Component {
   constructor(props) {
@@ -58,8 +59,24 @@ class Counter extends Component {
         />
         <CounterControl
           label="Subtract 5"
-          clicked={this.props.onSubtractCounter}
+          clicked={() => this.props.onSubtractCounter(5)}
         />
+        <hr />
+        <button type="submit" onClick={this.props.onStoreResult}>
+          Store Results
+        </button>
+        <ul>
+          {this.props.storedResults.map((storedResult) => {
+            return (
+              <li
+                key={storedResult.id}
+                onClick={() => this.props.onDeleteResult(storedResult.id)}
+              >
+                {storedResult.value}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
@@ -68,15 +85,20 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
   return {
     ctr: state.counter,
+    storedResults: state.results,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-    onAddCounter: (e) => dispatch({ type: "ADD", value: e }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 5 }),
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: (val) => dispatch({ type: actionTypes.ADD, value: val }),
+    onSubtractCounter: (val) =>
+      dispatch({ type: actionTypes.SUBTRACT, value: val }),
+    onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+    onDeleteResult: (id) =>
+      dispatch({ type: actionTypes.DELETE_RESULT, value: id }),
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,67 @@
+/*  We should not mutate state direcly bcz in JS, Object is of reference type 
+    So, direcly accessing "state" variable here, its better to create a clone or copy and then
+    operate on that. For Ex:-
+    
+    const newState = state;
+    newState.counter = counter + 1;
+    return newState;
+
+    In above example, many gets confuse that we copy state variable and operate on newState variable
+    but technically you are operating on state object itself. Because you are just pointing to same Object
+*/
+
+/*
+    In JS, there are two ways to create copy of that:-
+    1. Object.assign({}, state);
+    2. {...state} :- this is called Object Destructuring
+
+    ***************
+    Belowe I am doing in both ways, in increment I am using assign and decrement, I am using destructuring.
+    One unique thing, one can notice in destructing way is that, we are updating counter with some value
+    but now the question is what about existing counter property in state.So, in that case, while destructuring
+    object, if property is same, it will just update the property and rest are same.
+*/
+
+import * as actionTypes from "../store/actions";
+
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case actionTypes.INCREMENT:
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
+    case actionTypes.DECREMENT:
       return {
-        counter: state.counter + 1,
+        ...state,
+        counter: state.counter - 1,
       };
-    case "DECREMENT":
+    case actionTypes.ADD:
       return {
-        counter: state.counter + 1,
-      };
-    case "ADD":
-      return {
+        ...state,
         counter: state.counter + action.value,
       };
-    case "SUBTRACT":
+    case actionTypes.SUBTRACT:
       return {
+        ...state,
         counter: state.counter - action.value,
+      };
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: Date.now(), value: state.counter }),
+      };
+    case actionTypes.DELETE_RESULT:
+      // We use filter because filter method will create new array and will not touch the
+      // actual actual. So, it is best way to delete an item.
+      let updatedArray = state.results.filter((row) => row.id !== action.value);
+      return {
+        ...state,
+        results: updatedArray,
       };
 
     default:
